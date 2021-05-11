67599b50f0ec3bbff16b5a7f7cc6c1d104859762
ncchen99 <ncchen99@gmail.com>
2021-05-12 00:08:04 +0800

    📩

--- crawler/get_resource.py
+++ crawler/get_resource.py
@@ -2,6 +2,7 @@ import requests
 import os
 import json
 import urllib.parse
+import re
 from bs4 import BeautifulSoup
 from pprint import pprint
 
@@ -9,12 +10,18 @@ from pprint import pprint
 class Widget:
     def __init__(self, sessid, request_urls):
         self.headers = {
-            'referer': 'https://www.whsh.tc.edu.tw/ischool/widget/site_news/news_pop_content.php?newsId=14028&maxRows_rsResult=15&fh=0&bid=0&uid=WID_0_2_518cd2a7e52b7f65fc750eded8b99ffcc2a7daca', 'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+            'referer': 'https://www.whsh.tc.edu.tw/ischool/widget/', 'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
             'sec-ch-ua-mobile': '?0',
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'}
 
         self.cookie = {"PHPSESSID": sessid}
         self.request_urls = request_urls
+        self.percent_encode = {"%21": "!", "%23": "#", "%24": "$", "%26": "&", "%27": "'", "%28": "(", "%29": ")", "%2A": "*", "%2B": "+", "%2C": ",", "%2F": "/",
+                               "%3A": ":", "%3B": ";", "%3D%": "=", "%3F": "?", "%40": "@", "%5B": "[", "%5D": "]", "%20": " "}
+
+    def hex_to_char(self, hex_str):
+        """ converts a single hex-encoded character 'FFFF' into the corresponding real character """
+        return chr(int(hex_str, 16))
 
     def get_all_img(self, news, board):
         url = self.request_urls["base_url"] + \
@@ -28,16 +35,38 @@ class Widget:
         imgs = soup.findAll('img')
         imgs_list = []
         for img in imgs:
-            if "https://www.whsh.tc.edu.tw/ischool/" in img["src"]:
+            if self.request_urls["base_url"] in img["src"]:
                 imgs_list.append(img["src"])
         return imgs_list
 
     def get_all_attachment(self, news, board):
-        print("under dev!")
+        url = self.request_urls["base_url"] + \
+            self.request_urls["query_news_content"]["general"]["request_url"]
+        data = self.request_urls["query_news_content"][board]["query_string_parameters"]
+        data["nid"] = news["newsId"]
+        r = requests.get(url, params=data)
+        res_dict = json.loads(r.text)
+
+        attachment_list = []
+        attachments = json.loads(res_dict[0]["attachedfile"])
+        for attachment in attachments:
+            match_percent_decode = re.findall(
+                r"%([0-9a-fA-F]{2})", attachment[-1])
+            for sign in match_percent_decode:
+                attachment[-1] = attachment[-1].replace(
+                    "%"+sign, self.percent_encode["%"+sign])
+            attachment_list.append(
+                self.request_urls["base_url"]
+                + "resources/"+res_dict[0]["uid"] + "/"
+                + res_dict[0]["resources"] + "/attached/"
+                + re.compile(r"%u([0-9a-fA-F]{4})").sub(
+                    lambda m: self.hex_to_char(m.group(1)), attachment[-1]))
+            print(attachment_list[-1])
+        return attachment_list
 
     def save_img(self, url):
         dirs = url.replace(
-            "https://www.whsh.tc.edu.tw/ischool/", "").split("/")[:-1]
+            self.request_urls["base_url"], "").split("/")[:-1]
         path = dirs[0]
         for dir in dirs[1:]:
             if not os.path.exists(path):
@@ -47,12 +76,27 @@ class Widget:
             os.makedirs(path)
 
         print("downloading: ", url.replace(
-            "https://www.whsh.tc.edu.tw/ischool/resources/", ""), end=" ")
+            self.request_urls["base_url"] + "resources/", ""), end=" ")
         r = requests.get(url, headers=self.headers, cookies=self.cookie)
-        with open(url.replace("https://www.whsh.tc.edu.tw/ischool/", ""), 'wb') as f:
+        with open(url.replace(self.request_urls["base_url"], ""), 'wb') as f:
             # 將圖片下載下來
             f.write(r.content)
         print("... done")
 
-    def save_attachment(self, attachment):
-        print("under dev!!")
+    def save_attachment(self, url):
+        dirs = url.replace(
+            self.request_urls["base_url"], "").split("/")[:-1]
+        path = dirs[0]
+        for dir in dirs[1:]:
+            if not os.path.exists(path):
+                os.makedirs(path)
+            path += "/" + dir
+        if not os.path.exists(path):
+            os.makedirs(path)
+        print("downloading: ", url.replace(
+            self.request_urls["base_url"] + "resources/", ""), end=" ")
+        r = requests.get(url, headers=self.headers, cookies=self.cookie)
+        with open(url.replace(self.request_urls["base_url"], ""), 'wb') as f:
+            # 將圖片下載下來
+            f.write(r.content)
+        print("... done")
--- main.py
+++ main.py
@@ -6,11 +6,11 @@ news_widget = get_news.Widget(PHPSESSID, request_urls)
 resource_widget = get_resource.Widget(PHPSESSID, request_urls)
 query_boards = request_urls["board"].keys()
 for board in query_boards:
-    news_list = news_widget.get_news_list(board, 20)
+    news_list = news_widget.get_news_list(board, 100)
     for news in news_list[1:]:
         img_list = resource_widget.get_all_img(news, board)
         for img in img_list:
             resource_widget.save_img(img)
-        # attachment_list = resource_widget.get_all_attachment(news, board)
-        # for attachment in attachment_list:
-        #     resource_widget.save_attachment(attachment)
+        attachment_list = resource_widget.get_all_attachment(news, board)
+        for attachment in attachment_list:
+            resource_widget.save_attachment(attachment)
